e29ba117c3dbaedf94d7db992e861e48d2ae2f69
dabarov <simlyalde@gmail.com>
2022-10-09 17:56:18 +0600

    fix: change chat_id type to bigint

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE aliases
 (
     id            SERIAL PRIMARY KEY,
-    chat_id       INT          NOT NULL,
+    chat_id       BIGINT       NOT NULL,
     sticker_id    VARCHAR(100) NOT NULL,
     sticker_alias VARCHAR(100) NOT NULL,
     UNIQUE (chat_id, sticker_alias)
